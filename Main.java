3edb78b192f83c24175932f4212727f895756d3c
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-05-05 19:12:35 -0300

    up

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Olá Mundoooo!!!");
+    public static void main(String[] args) { // declaração da classe + nome da classe
+
+        System.out.println("My Messagdde"); // método
     }
 }
