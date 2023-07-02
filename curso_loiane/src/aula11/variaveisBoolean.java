ebc813df7e61be043f449c376511c4b0ebd0f4a0
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-02 00:37:31 -0300

    Create variaveisBoolean.java
    up

---
+++ curso_loiane/src/aula11/variaveisBoolean.java
@@ -0,0 +1,13 @@
+package aula11;
+
+public class variaveisBoolean {
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+
+        boolean falso = false;
+
+        System.out.println("O valor de Verdadeiro é = " + verdadeiro);
+        System.out.println("O valor de Falso é = " + falso);
+    }
+}
