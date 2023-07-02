05e10a2fd25d012d0dd9c215cc509eafe0eeab7d
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-02 00:37:23 -0300

    up

---
+++ curso_loiane/src/aula11/curiosidadeInt.java
@@ -0,0 +1,12 @@
+package aula11;
+
+public class curiosidadeInt {
+    public static void main(String[] args) {
+
+        int var1 = 2147483647;
+        int var2 = 100;
+
+        System.out.println(var1 + var2);
+
+    }
+}
