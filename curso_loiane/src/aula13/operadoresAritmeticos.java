e30cf7f6fbf36873d9b4b646ef0691497463682b
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-02 23:53:51 -0300

    up

---
+++ curso_loiane/src/aula13/operadoresAritmeticos.java
@@ -0,0 +1,42 @@
+package aula13;
+
+public class operadoresAritmeticos {
+    public static void main(String[] args) {
+
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado -= 1;
+        System.out.println(resultado);
+
+        resultado *= 2;
+        System.out.println(resultado);
+
+        resultado /= 2;
+        System.out.println(resultado);
+
+        resultado += 8;
+        System.out.println(resultado);
+
+        resultado %= 7;
+        System.out.println(resultado);
+
+        String primeiroNome = "Esta é";
+        String segundoNome = " uma string concatenada";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println(terceiroNome);
+
+        resultado += 1;
+        System.out.println(resultado);
+
+        resultado++;  //add 1
+        System.out.println(resultado);
+
+        // 5
+        System.out.println(resultado++); // 5
+        // mesma coisa que
+        //System.out.println(resultado);
+        //resultado += 1;
+        System.out.println(++resultado); // 7
+    }
+}
