daccfdc2bc4f50034f687a3b9d8238146fe06282
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-03 00:11:00 -0300

    up

---
+++ curso_loiane/src/aula13/operadoresRelacionais.java
@@ -0,0 +1,16 @@
+package aula13;
+
+public class operadoresRelacionais {
+    public static void main(String[] args) {
+
+        int valor1 = 1;
+        int valor2 = 2;
+
+        System.out.println("valor1 == valor2: " + (valor1 == valor2));
+        System.out.println("valor1 != valor2: " + (valor1 != valor2));
+        System.out.println("valor1 > valor2: " + (valor1 > valor2));
+        System.out.println("valor1 >= valor2: " + (valor1 >= valor2));
+        System.out.println("valor1 < valor2: " + (valor1 < valor2));
+        System.out.println("valor1 <= valor2: " + (valor1 <= valor2));
+    }
+}
