6c9c68a5e6d7d75f32bdb7b9dbdaccd074b6fc55
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-03 22:18:50 -0300

    up

---
+++ curso_loiane/src/aula13/curtoCircuito.java
@@ -0,0 +1,13 @@
+package aula13;
+
+public class curtoCircuito {
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+        boolean result1 = falso & verdadeiro;
+        boolean result2 = falso && verdadeiro;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
