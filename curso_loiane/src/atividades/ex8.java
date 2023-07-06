14039a14ef045f6b59c761d6b3a460dfaa1444c2
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-06 02:39:17 -0300

    done

--- curso_loiane/src/atividades/ex8.java
+++ curso_loiane/src/atividades/ex8.java
@@ -1,7 +1,19 @@
 package atividades;
 
+import java.util.Scanner;
+
 public class ex8 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor por hora: ");
+        int valorPorHora = sc.nextInt();
+
+        System.out.println("Total de horas de trabalho no mês: ");
+        float horasTrabalhadas = sc.nextFloat();
+
+        float salarioTotal = valorPorHora * horasTrabalhadas;
+
+        System.out.printf("TOTAL: %.2f\n", salarioTotal);
     }
 }
