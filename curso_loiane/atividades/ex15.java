f14a078a7613b0475eaa1a19be4f949d560e425c
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-10 01:23:30 -0300

    att

--- curso_loiane/atividades/ex15.java
+++ curso_loiane/atividades/ex15.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 
 public class ex15 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("SALÁRIO BRUTO: ");
-        System.out.println("VALOR INSS: ");
-        System.out.println("VALOR SINDICATO: ");
-        System.out.println("SALÁRIO LIQUIDO: ");
-        System.out.println("");
+        System.out.print("Quanto você ganha por hora: ");
+        float salarioHora = sc.nextFloat();
+
+        System.out.print("Número de horas trabalhadas no mês: ");
+        int horasTrabalhadas = sc.nextInt();
+
+        float bruto = salarioHora * horasTrabalhadas;
+        float impostoDeRenda = bruto * 0.11f;
+        float inss = bruto * 0.08f;
+        float sindicato = bruto * 0.05f;
+        float total = impostoDeRenda + inss + sindicato;
+        float liquido = bruto - total;
+
+        System.out.printf("SALÁRIO BRUTO: %.2f\n", bruto);
+        System.out.printf("IMPOSTO DE RENDA: %.2f\n", impostoDeRenda);
+        System.out.printf("VALOR INSS: %.2f\n", inss);
+        System.out.printf("VALOR SINDICATO: %.2f\n", sindicato);
+        System.out.printf("SALÁRIO LIQUIDO: %.2f\n", liquido);
+
+        sc.close();
     }
 }
