dd8b58c7f3f8afc0993f8bd947163154a4696a30
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-09 23:46:54 -0300

    att

---
+++ curso_loiane/atividades/ex15.java
@@ -0,0 +1,16 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class ex15 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("SALÁRIO BRUTO: ");
+        System.out.println("VALOR INSS: ");
+        System.out.println("VALOR SINDICATO: ");
+        System.out.println("SALÁRIO LIQUIDO: ");
+        System.out.println("");
+    }
+}
