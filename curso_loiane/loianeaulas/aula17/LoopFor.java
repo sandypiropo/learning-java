e74b44cb86f6bdb496f85d91673fc6b5465e345d
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-16 20:37:52 -0300

    att

--- curso_loiane/loianeaulas/aula17/LoopFor.java
+++ curso_loiane/loianeaulas/aula17/LoopFor.java
@@ -1,4 +1,32 @@
 package loianeaulas.aula17;
 
 public class LoopFor {
+    public static void main(String[] args) {
+
+        for (int i=0; i < 5; i++) {
+            System.out.println("I = " + i);
+        }
+        System.out.println("---");
+        for (int i=5; i >= 0; i--) {
+            System.out.println("I = " + i);
+        }
+
+        for (int i = 0, j = 10; i <= j; i++, j--) {
+            System.out.println("i = " + i + "; j = " + j);
+        }
+
+        int count = 0;
+        for ( ; count < 5 ; ) {
+            System.out.println("valor de count = " + count);
+            count += 2;
+        }
+
+        for (int cont = 0; cont < 10; cont += 2) {
+            System.out.println("valor de cont = " + cont);
+        }
+
+        int soma = 0;
+        for (int i=1; i < 5; soma += i++);
+        System.out.println(soma);
+    }
 }
---
+++ meus_projetos/projetoavulso2/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+F
+src/aula04/Caneta.java,2\5\25d60197579be9196dade2b7108a7ccd10695d83
+D
+src/aula04/Main.java,7\a\7a2b6661e1f3554c5b16ccfd5a5e71bb030c81a5
