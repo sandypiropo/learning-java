26708e95837c23cbfd06f64ba6f0e56b8ea7a1dc
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-18 17:32:39 -0300

    att

--- curso_loiane/atividades/aulas14-15/Ex02.java
+++ curso_loiane/atividades/aulas14-15/Ex02.java
@@ -1,4 +1,12 @@
 package atividades.aulas1415;
 
+import java.util.Scanner;
+
 public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int valor = scanner.nextInt();
+        System.out.println((valor < 0)?"Negativo":"Positivo");
+    }
 }
