af8ac07c4db0a5c8e574b1a89343187bebd724b6
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-07-21 22:58:04 -0300

    att

--- curso_loiane/atividades/aulas14-15/Ex02.java
+++ curso_loiane/atividades/aulas14-15/Ex02.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class Ex02 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        int valor = scanner.nextInt();
-        System.out.println((valor < 0)?"Negativo":"Positivo");
-    }
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero > 0) {
+            System.out.println("O número digitado é positivo.");
+        } else if (numero < 0) {
+            System.out.println("O número digitado é negativo.");
+        } else {
+            System.out.println("O número digitado é zero.");
+        }
+
+        scanner.close();
+}
 }
