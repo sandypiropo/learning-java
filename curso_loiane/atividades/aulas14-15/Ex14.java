f7e6cb72576768cb0b162a989515d2fbb0b21ab9
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-08-07 23:50:01 -0300

    Create Ex14.java

---
+++ curso_loiane/atividades/aulas14-15/Ex14.java
@@ -0,0 +1,47 @@
+package atividades.aulas1415;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double nota1 = -1;
+        double nota2 = -2;
+        boolean valoresValidos = false;
+
+        do {
+            try{
+                System.out.print("Primeira nota: ");
+                nota1 = scanner.nextDouble();
+                System.out.print("Segunda nota: ");
+                nota2 = scanner.nextDouble();
+
+                if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10) {
+                    System.out.println("Inválido escreva novamente");
+                } else {
+                    valoresValidos = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Valor inválido. Digite um número válido.");
+                scanner.nextLine(); // Limpar o buffer do scanner
+            }
+        } while (!valoresValidos);
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media < 4.0) {
+            System.out.println("E");
+        } else if (media < 6.0) {
+            System.out.println("D");
+        } else if (media < 7.5) {
+            System.out.println("C");
+        } else if (media < 9.0) {
+            System.out.println("B");
+        } else {
+            System.out.println("A");
+        }
+        scanner.close();
+    }
+}
