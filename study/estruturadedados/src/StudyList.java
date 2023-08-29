53242445d6637d662f3386eb06400cc4fde4ca1f
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-08-29 00:13:25 -0300

    Create StudyList.java

---
+++ study/estruturadedados/src/StudyList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudyList {
+    public static void main(String[] args) {
+
+        List<Integer> lista;
+        lista = new ArrayList<>();
+
+        lista.add(1);
+        lista.add(3);
+        lista.add(null);
+        lista.add(null);
+        lista.add(1);
+
+        print(lista);
+    }
+
+    private static void print(List<Integer> item) {
+        for (int i = 0 ; i < item.size(); i++) {
+            System.out.println(item.get(i));
+        }
+    }
+}
