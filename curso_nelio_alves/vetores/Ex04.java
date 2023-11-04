81d96230c5ecbd3926b95e3af4b7008144631b44
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-11-04 10:02:03 -0300

    Create Ex04.java

---
+++ curso_nelio_alves/vetores/Ex04.java
@@ -0,0 +1,31 @@
+package vetores;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many numbers?: ");
+        int number = sc.nextInt();
+
+        int even = 0;
+        int[] numberList = new int[number];
+        ArrayList<Integer> evenNumber = new ArrayList<>();
+
+        for (int i=0 ; i<numberList.length; i++) {
+            System.out.println("Digite um numero: ");
+            numberList[i] = sc.nextInt();
+            if (numberList[i] % 2 == 0) {
+                even++;
+                evenNumber.add(numberList[i]);
+            }
+        }
+
+        System.out.println("EVEN NUMBERS: " + evenNumber);
+        System.out.println("EVEN TOTAL: " + even);
+
+        sc.close();
+    }
+}
